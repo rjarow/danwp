165c633cde45e4f6bbfa5ac1ee01a3bb4f3707c3
Rich J <rjarow@gmail.com>
2017-09-17 18:29:08 -0400

    Changing run.sh logic.

--- files/run.sh
+++ files/run.sh
@@ -5,18 +5,27 @@
 # Install wp-cli if it doesn't exist.
 if [ ! -f /usr/bin/wp ]; then
   curl -O https://raw.githubusercontent.com/wp-cli/builds/gh-pages/phar/wp-cli.phar && chmod +x wp-cli.phar && mv wp-cli.phar /usr/bin/wp && chmod 755 /usr/bin/wp && chown nginx:nginx /usr/bin/wp
+else
+  echo "wp-cli already installed"
 fi
 
 # If /usr/html/wp-config.php doesn't exist (wordpress not installed) -> take variables from Docker/Compose and create configuration.
 if [ ! -f /usr/html/wp-config.php ]; then
   mkdir -p /usr/html
   cd /usr/html
-  chown -R nginx:nginx /usr/html
   wp core download --path=/usr/html
+else
+  echo "WP already installed."
+fi
+
+if [ -n "$WORDPRESS_DB" ]; then
   /usr/bin/wait-for-it.sh ${WORDPRESS_DB_HOST}:3306 --timeout=60 --strict -- wp config create --dbhost=$WORDPRESS_DB_HOST --dbname=$WORDPRESS_DB --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --path=/usr/html
 else
-  chown -R nginx:nginx /usr/html
+  echo "WP already configured"
 fi
+
+# chown /usr/html
+chown -R nginx:nginx /usr/html
 
 # Start php-fpm
 mkdir -p /usr/logs/php-fpm
