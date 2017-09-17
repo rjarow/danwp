e89cbbba2eca69f3b5f4eef5abd3f7a8a08ddc7e
Rich J <rjarow@gmail.com>
2017-09-17 18:54:19 -0400

    bash is fun?

--- files/run.sh
+++ files/run.sh
@@ -19,7 +19,7 @@ else
 fi
 
 # If variables are set and wp-config.php isn't in place ,create wp-config.php, otherwise leave it be.
-if [ -n "$WORDPRESS_DB" ] && [ ! -f /usr/html/wp-config.php ]; then
+if [ -n $WORDPRESS_DB ] && [ ! -f /usr/html/wp-config.php ]; then
   /usr/bin/wait-for-it.sh ${WORDPRESS_DB_HOST}:3306 --timeout=60 --strict -- wp config create --dbhost=$WORDPRESS_DB_HOST --dbname=$WORDPRESS_DB --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --path=/usr/html
 else
   echo "WP either configured or Environment Variables not set"
