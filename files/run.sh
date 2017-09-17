577d2159afaa93a0bacd2c5b229ac2e7e32bf731
Rich J <rjarow@gmail.com>
2017-09-17 01:08:59 -0400

    Added longer sleep to wait for db to come up I eventually should use wait-for.sh, this is a temp fix.

--- files/run.sh
+++ files/run.sh
@@ -12,7 +12,7 @@ if [ ! -f /usr/html/wp-config.php ]; then
   mkdir -p /usr/html
   cd /usr/html
   chown -R nginx:nginx /usr/html
-  su -c "wp core download --path=/usr/html && wp config create --dbhost=$WORDPRESS_DB_HOST --dbname=$WORDPRESS_DB --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --path=/usr/html" nginx
+  su -c "wp core download --path=/usr/html && sleep 10 && wp config create --dbhost=$WORDPRESS_DB_HOST --dbname=$WORDPRESS_DB --dbuser=$WORDPRESS_DB_USER --dbpass=$WORDPRESS_DB_PASSWORD --path=/usr/html" nginx
 else
   chown -R nginx:nginx /usr/html
 fi
